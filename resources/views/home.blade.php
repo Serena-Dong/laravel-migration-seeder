aef59bfe407a806d3cccfc1b4815d9ac24727922
Serena-Dong <serenadong3159@icloud.com>
2023-02-27 18:31:12 +0100

    style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ env('APP_NAME') }}</title>
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
 </head>
 <body>
     
